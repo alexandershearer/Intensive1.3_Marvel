2a22c724d296b46e498bf9a8e542e7e9cbea528e
alexandershearer <alex.shearer23@gmail.com>
2021-03-20 17:17:39 -0500

    styled comic search

--- src/Comics/Components/ComicSearch/comicSearch.js
+++ src/Comics/Components/ComicSearch/comicSearch.js
@@ -2,10 +2,16 @@ import React, { Component } from 'react'
 import { withStyles, TextField, Button } from '@material-ui/core'
 import md5 from 'md5'
 import ComicsContainer from '../ComicsContainer/comicsContainer'
+import './comicSearch.css'
 
 
 const styles = ({
-
+    searchComic: {
+        paddingTop: '40px'
+    },
+    searchButton: {
+        marginBottom: '20px'
+    }
 })
 
 class ComicSearch extends Component {
@@ -60,7 +66,12 @@ class ComicSearch extends Component {
         const { title, comicInfo, error } = this.state
         const { classes } = this.props
         return (
-            <div className={classes.searchHero}>
+            <div className={classes.searchComic}>
+                <div>
+                    <h2>There are thousands of comics in the Marvel world! Some of them starring a character and others having a handful
+                    of your favorite characters. Search through Marvel's comic library to find info your future reads!
+                    </h2>
+                </div>
                 <div className={classes.form}>
                     <TextField
                         className={classes.userInput}
@@ -81,6 +92,12 @@ class ComicSearch extends Component {
                     </Button>
                 </div>
                 {error ? error : <ComicsContainer comicInfo={comicInfo} />}
+                <div>
+                    <img className='comicHeaderImg' src='https://i.pinimg.com/736x/96/98/a5/9698a5d48b9fdb3a9bb49f1e5fa8958d.jpg' />
+                    <img className='comicHeaderImg' src='https://i.pinimg.com/736x/13/8a/b1/138ab1588ee061065f626c726705a39b.jpg' />
+                    <img className='comicHeaderImg' src='https://qph.fs.quoracdn.net/main-qimg-afbc5ab63f9ebbe1cb1cc1c7ddac0fd3' />
+                    <img className='comicHeaderImg' src='https://i.pinimg.com/originals/c8/7b/b4/c87bb4f13f03cb64815435215495204a.jpg' />
+                </div>
             </div >
         )
     }
--- src/Comics/Components/ComicsContainer/comicsContainer.js
+++ src/Comics/Components/ComicsContainer/comicsContainer.js
@@ -2,7 +2,11 @@ import React, { Component } from 'react'
 import { withStyles } from '@material-ui/core'
 
 const styles = ({
-
+    comicImage: {
+        width: '400px',
+        height: '550px',
+        border: '1px solid black',
+    }
 })
 
 class ComicsContainer extends Component {
@@ -10,10 +14,10 @@ class ComicsContainer extends Component {
     render() {
         const { classes } = this.props
         return (
-            <div>
+            <div className={classes.comicContainer}>
                 {this.props.comicInfo.title !== '' ?
                     <div>
-                        <img className={classes.heroImage} src={this.props.comicInfo.image} alt='char' />
+                        <img className={classes.comicImage} src={this.props.comicInfo.image} alt='char' />
                         <h2>{this.props.comicInfo.title}</h2>
                         <p>{this.props.comicInfo.pages}</p>
                         <p>{this.props.comicInfo.desc}</p>
